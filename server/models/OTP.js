1a3c646031499e681a8b674ab96bdba8d6815750
Oitash <oitashg@gmail.com>
2024-08-20 15:25:08 +0530

    Models for sending mails and otp are created

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const mailSender = require("../utils/mailSender")
 
 const OTPSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,24 @@ const OTPSchema = new mongoose.Schema({
         default: Date.now(),
         expires: 5*60,
     }
+})
+
+// function to send otp to email as a purpose of verification
+async function sendVerificationEmail(email, otp) {
+    try{
+        const mailResponse = await mailSender(email, "Verification Email from Codeholic", otp)
+        console.log("Email sent successfully: ", mailResponse)
+    }
+    catch(error){
+        console.log("Error occured while sending mails: ", error)
+        throw error
+    }
+}
+
+//pre middleware...send verification email just before saving the document
+OTPSchema.pre("save", async function(next) {
+    await sendVerificationEmail(this.email, this.otp)
+    next()
 })
 
 module.exports = mongoose.model("OTP", OTPSchema)
