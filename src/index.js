b19760ce5f12dd9ae48803f8242dfede481f35da
Oitash <oitashg@gmail.com>
2024-09-06 12:35:46 +0530

    :tada: Started building UI....

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* The app component is wrapped in browser router to create routes
+    for every pages and components */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
