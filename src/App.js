ad6af8a44b607a6f6910ca35b03a2122ce1da653
Oitash <oitashg@gmail.com>
2024-09-06 12:41:06 +0530

    :hammer: Some changes are made in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div>
-      
+    <div className='w-screen min-h-screen bg-richblack-900 flex-col font-inter'>
+      {/* Under BrowserRouter, there is "Routes" then under that there are many individual "Route" */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
