0c341af7512b96dbb8f3b18ad12dd3b143fa996b
Oitash <oitashg@gmail.com>
2024-09-17 10:17:36 +0530

    All slices are combined in reducer....

---
+++ src/reducer/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from "@reduxjs/toolkit";
+
+import authReducer from "../slices/authSlice"
+import profileReducer from "../slices/profileSlice"
+import cartReducer from "../slices/cartSlice"
+import courseReducer from '../slices/courseSlice'
+import viewCourseReducer from '../slices/viewCourseSlice'
+
+//There will be many reducers, so all reducers are combined into rootReducer
+//rootReducer is passed into store in index.js
+//Reducers are made from slices
+//So we will add/combine reducers here one by one 
+const rootReducer = combineReducers({
+    auth: authReducer,
+    profile: profileReducer,
+    cart: cartReducer,
+    course: courseReducer,
+    vewCourse: viewCourseReducer,
+})
+
+export default rootReducer
