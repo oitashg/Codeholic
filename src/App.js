2246f1326c42f87f2d74f16abe952ed9f3f16299
Oitash <oitashg@gmail.com>
2024-09-17 10:22:13 +0530

    :hammer: These are being updated simutaneously....

--- src/App.js
+++ src/App.js
@@ -1,13 +1,134 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home"
+import Navbar from './components/common/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import OpenRoute from './components/core/Auth/OpenRoute';
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+import Error from './pages/Error';
+import ForgotPassword from './pages/ForgotPassword';
+import UpdatePassword from './pages/UpdatePassword';
+import VerifyEmail from './pages/VerifyEmail';
+import MyProfile from './components/core/Dashboard/MyProfile';
+import Dashboard from './pages/Dashboard';
+import Settings from './components/core/Dashboard/Settings';
+import EnrolledCourses from './components/core/Dashboard/EnrolledCourses';
+import Cart from './components/core/Dashboard/Cart';
+import { useSelector } from 'react-redux';
+import { ACCOUNT_TYPE } from './utils/constants';
+import AddCourse from './components/core/Dashboard/AddCourse';
 
 function App() {
+
+  const {user} = useSelector((state) => state.profile)
+  
   return (
     <div className='w-screen min-h-screen bg-richblack-900 flex-col font-inter'>
       {/* Under BrowserRouter, there is "Routes" then under that there are many individual "Route" */}
+      {/* Navbar is common for all pages, it will be at the top. So, it is written in App.js */}
+      <Navbar/>
+
+      {/* All routes should be added in App.js only for moving from one page to other */}
+      {/* Routes directing to respective pages are given here */}
       <Routes>
         <Route path='/' element={<Home/>}/>
+
+        {/* Wrapped in openRoute so that everyone can visit */}
+        <Route 
+          path='/login' 
+          element={
+            <OpenRoute>
+              <Login/>
+            </OpenRoute>
+          }
+        />
+
+        {/* Wrapped in openRoute so that everyone can visit */}
+        <Route 
+          path='/signup' 
+          element={
+            <OpenRoute>
+              <Signup/>
+            </OpenRoute>
+          }
+        />
+        
+        <Route 
+          path='/forgot-password' 
+          element={
+            <OpenRoute>
+              <ForgotPassword/>
+            </OpenRoute>
+          }
+        />
+
+        <Route 
+          path='/verify-email' 
+          element={
+            <OpenRoute>
+              <VerifyEmail/>
+            </OpenRoute>
+          }
+        />
+
+        <Route 
+          path='/update-password/:id' 
+          element={
+            <OpenRoute>
+              <UpdatePassword/>
+            </OpenRoute>
+          }
+        />
+
+        {/* About and contact must be kept as a normal route as it can be visited by any user at any time 
+        whether logged in or logged out or authorized or unauthorized */}
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        
+        {/* Dashboard route has other nested routes
+        Parent is dashboard inside which there are other routes
+        Dashboard comes under PrivateRoute as other people can't access this */}
+        <Route 
+          element={
+            <PrivateRoute>
+              <Dashboard/>
+            </PrivateRoute>
+          }
+        >
+          <Route path='dashboard/my-profile' element={<MyProfile/>}/>
+          <Route path='dashboard/settings' element={<Settings/>}/>
+          
+          {
+            //If this validation is not added, then instructor can also open ebrolled-courses and cart page which is logically wrong
+            //That's why this validation is added
+            //Always add ? to ensure whether accountType is present or not
+            user?.accountType === ACCOUNT_TYPE.STUDENT && (
+              <>
+                <Route path='dashboard/enrolled-courses' element={<EnrolledCourses/>}/>
+                <Route path='dashboard/cart' element={<Cart/>}/>
+              </>
+            )
+          }
+
+
+          {
+            //If this validation is not added, then instructor can also open ebrolled-courses and cart page which is logically wrong
+            //That's why this validation is added
+            //Always add ? to ensure whether accountType is present or not
+            user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+              <>
+                <Route path='dashboard/add-course' element={<AddCourse/>}/>
+              </>
+            )
+          }
+
+        </Route>
+
+        {/* If unknown route then show error page */}
+        <Route path='*' element={<Error/>}/>
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import {configureStore} from "@reduxjs/toolkit"
+import rootReducer from './reducer';
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+
+//Central store is created here using configureStore and rootReducer is passed as reducer
+//which contains/combines all reducers
+const store = configureStore({
+  reducer: rootReducer
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* The app component is wrapped in browser router to create routes
-    for every pages and components */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* App to be wrapped by Provider in 0rder to use store, reducer and slices */}
+    <Provider store={store}>
+      {/* The app component is wrapped in browser router to create routes
+      for every pages and components */}
+      <BrowserRouter>
+        <App />
+        <Toaster/>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
