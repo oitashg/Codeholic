40523ad6e90b2d1c5ea6f832eb49b6a6443509fa
Oitash <oitashg@gmail.com>
2024-12-06 12:34:49 +0530

    Working....

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "https://codeholic.vercel.app/",
+        origin: "https://codeholic.vercel.app",
         credentials: true,
     })
 )
