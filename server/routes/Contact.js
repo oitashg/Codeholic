55e45d1d8b6cb7dc361af492f6fffcbca872ab86
Oitash <oitashg@gmail.com>
2025-01-26 18:29:37 +0530

    :bug: Debugging...

--- server/routes/Contact.js
+++ server/routes/Contact.js
@@ -2,6 +2,6 @@ const express = require("express")
 const router = express.Router()
 const { contactUsController } = require("../controllers/ContactUs")
 
-router.post("/contact", contactUsController)
+router.post("/reach/contact", contactUsController)
 
 module.exports = router
